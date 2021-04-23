16cf844ef4cbbd11857fe12ed444491141c5f298
yuchehsieh <hsiehdanny860605@gmail.com>
2021-04-23 20:54:55 +0800

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import styles from './styles.module.scss';
 const Header = () => {
   return (
     <div className={styles.container}>
-      <h1>數位系 DTD 使用 DOCKER WORDPRESS HOT-RELOAD~!~~~~~!~</h1>
+      <h1>數位系 DTD 使用 DOCKER WORDPRESS 從網路上更新</h1>
     </div>
   );
 };
